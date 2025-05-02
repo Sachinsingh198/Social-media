0dc8c021a05fdebbef9fa4577aa4f590cb06d25c
Sachinsingh198 <Sachinsinghfarswan123@gmail.com>
2025-05-02 22:14:43 +0530

    Added missing components folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-
+import {useState} from 'react';
+import Header from "./component/header.js";
+import Footer from "./component/footer.js";
+import SideBar from "./component/sidebar.js";
+import CreatePost from "./component/createPost.js";
+import PostList from "./component/postList.js";
+import PostListProvider from './store/post-list-store.js';
 function App() {
+  
+  const [selectedTab, setSelectedTab] = useState("Home");
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <PostListProvider>
+  <div className = "app-container">
+   <SideBar 
+   selectedTab = {selectedTab}
+   setSelectedTab = {setSelectedTab}
+   ></SideBar>
+   <div className = "content">
+   <Header/>
+   {selectedTab === "Home" ?  <PostList></PostList> : <CreatePost></CreatePost>}
+   <Footer/>
+   </div>
+   </div>
+   </PostListProvider>
+  </>
   );
 }
 
---
+++ src/store/post-list-store.js
@@ -0,0 +1,77 @@
+import {createContext, useReducer} from 'react';
+
+
+
+export const PostList = createContext({
+    postList: [],
+    addPost: () =>{},
+    deletePost: () => {},
+});
+const postListReducer = (currPostList, action)=>{
+    let newPostList = currPostList;
+    if(action.type === 'DELETE_POST'){
+        newPostList = currPostList.filter(post => post.id !== action.payload.postId)
+    }
+    else if (action.type === 'ADD_POST'){
+        newPostList = [action.payload, ...currPostList]
+    }
+    return newPostList; 
+};
+    
+const PostListProvider = ({children}) =>{
+    const [postList, dispatchPostList] = useReducer(
+        postListReducer, 
+        DEFAULT_POST_LIST
+    );
+
+
+    const addPost = (userId,postTitle,postBody,reactions,tags) => {
+        dispatchPostList({
+            type: 'ADD_POST',
+            payload: {
+                id: Date.now(),
+                title : postTitle,
+                body: postBody,
+                reaction: reactions,
+                userId: userId,
+                tags: tags
+            }
+        })
+    };
+
+
+    const deletePost = (postId) => {
+        dispatchPostList({
+            type: 'DELETE_POST',
+            payload:{
+                postId,
+            }
+        })
+    };
+    return( <PostList.Provider value = {{postList,addPost,deletePost}} >
+        {children}
+        </PostList.Provider>
+    );
+};
+
+const DEFAULT_POST_LIST = [
+    {
+        id: '1',
+        title : 'Going to Mumbai',
+        body: 'Hi Friends, I am going to Mumbai for my vacation. Hope to enjoy a lot. Peace Out',
+        reaction: 23,
+        userId: 'user-9',
+        tags: ['vacation', 'Mumbai', 'Enjoying']
+    },
+    {
+        id: '2',
+        title : 'Pass ho gaye Bhai',
+        body: '4 saal ki masti ke baad bhi ho gaye hain paas. Hard to believe',
+        reaction: 15,
+        userId: 'user-15',
+        tags: ['Graduating', 'unbelievable']
+    }
+
+]
+
+export default PostListProvider;
